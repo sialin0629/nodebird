30ca55360faea47a1c44cc1f5a5a0f5ac442306b
siain0629 <jemu0629@gmail.com>
2024-07-18 22:39:42 +0900

    nodebird 완성

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,6 @@ const router = express.Router(); // 새로운 라우터 객체 생성
 // 사용자 ID를 받아서 팔로우 요청 처리
 // isLoggedIn 미들웨어 사용 -> 로그인 여부 확인
 // follow 컨트롤러 호출 -> 팔로우 요청 처리
-router.post('/:id/following', isLoggedIn, follow);
+router.post('/:id/follow', isLoggedIn, follow);
 
 module.exports = router; // 라우터 객체를 모듈로 내보냄
